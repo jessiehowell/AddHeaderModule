f9cb811398aaa7c5b3ed8c2260d05ffe35d73dde
jessiehowell <34244976+jessiehowell@users.noreply.github.com>
2018-06-30 19:57:26 -0400

    Add files via upload

---
+++ AddHeader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Web;
+
+namespace AddHeaderModule
+{
+    public class AddHeader : IHttpModule
+    {
+        public void Dispose()
+        {
+        
+        }
+
+        public void Init(HttpApplication httpContext)
+        {
+            httpContext.PostAuthenticateRequest += new EventHandler(OnPostAuthenticateRequest);
+        }
+
+        public void OnPostAuthenticateRequest(Object source, EventArgs e)
+        {
+            HttpApplication httpApp = (HttpApplication)source;
+            HttpRequest httpReq = httpApp.Context.Request;
+            NameValueCollection customHeaders = ConfigurationManager.AppSettings;
+            foreach (string headerName in customHeaders)
+            {
+                string headerValue = customHeaders[headerName];
+                if (headerValue.StartsWith("{") && headerValue.EndsWith("}"))
+                {
+                    headerValue = httpReq.ServerVariables.Get(headerValue.TrimStart('{').TrimEnd('}'));
+                }
+                //NameValueCollection newHeader = new NameValueCollection();
+                try
+                {
+                    //newHeader.Set(headerName, headerValue);
+                    httpReq.Headers.Set(headerName, headerValue);
+                }
+                catch (Exception ex)
+                {
+                    //newHeader.Set("X-Exception-Thrown", ex.Source);
+                    httpReq.Headers.Set("X-Exception-Thrown", ex.Source);
+                }
+            }
+        }
+    }
+}
